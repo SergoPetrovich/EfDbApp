d2e99297ca4564e6e2687c1092ecbc6c63c41cb5
SP <tk34moodle@tk34.mskobr.ru>
2022-03-30 12:05:02 +0300

    Добавьте файлы проекта.

---
+++ Models/MobileContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+namespace EfDbApp.Models
+
+{
+
+    public class MobileContext : DbContext
+
+    {
+        public MobileContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Phone> Phones { get; set; }
+
+    }
+}
